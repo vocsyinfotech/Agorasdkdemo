81795420c9b739fab899f962dfb78a3dd562efd7
Kaushik Godhani <vocsydeveloper@gmail.com>
2021-04-14 15:55:32 +0530

    Initial commit

---
+++ lib/config/agora.config.dart
@@ -0,0 +1,14 @@
+/// Get your own App ID at https://dashboard.agora.io/
+const appId = "92155d8e11b347e89e6d59fcfff9e7b6";
+
+/// Please refer to https://docs.agora.io/en/Agora%20Platform/token
+const token = null;
+
+/// Your channel ID
+const channelId = "vocsy";
+
+/// Your int user ID
+const uid = 6449;
+
+/// Your string user ID
+const stringUid = "kaushik";
